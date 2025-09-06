2a672c4ce95dc4350c0319e9ab22c35a312e81f7
fluid-movement <derandre3000@hotmail.com>
2025-09-06 14:50:23 +0200

    feat: install Laravel Pulse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +18,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::define('viewPulse', function (User $user) {
+            return $user->isAdmin();
+        });
+
         if (config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
