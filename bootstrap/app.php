546fcea67f53450a6f60b9c8193df1689fb8e459
fluid-movement <derandre3000@hotmail.com>
2025-09-06 16:17:21 +0200

    feat: integrate Sentry for error tracking and monitoring

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,17 @@ use App\Http\Middleware\UpgradeToHttpsUnderNgrok;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(UpgradeToHttpsUnderNgrok::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        Integration::handles($exceptions);
     })->create();
