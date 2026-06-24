e13802dc868f8848f6f2c061349780b7888d265a
fluid-movement <derandre3000@hotmail.com>
2026-06-24 20:19:32 +0200

    Update app.php imports to use Laravel 11 namespacing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,9 @@
 <?php
 
-use IlluminateFoundationApplication;
-use IlluminateFoundationConfigurationExceptions;
-use IlluminateFoundationConfigurationMiddleware;
-use SentryLaravelIntegration;
-use SymfonyComponentHttpFoundationRequest;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
