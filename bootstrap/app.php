2a8ec84ab002278941d42bf77de2890afeb26f71
Andre Zaharias <andre.zaharias@proton.me>
2026-06-24 19:59:54 +0200

    Add api.php to route configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,15 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-use Sentry\Laravel\Integration;
-use Symfony\Component\HttpFoundation\Request;
+use IlluminateFoundationApplication;
+use IlluminateFoundationConfigurationExceptions;
+use IlluminateFoundationConfigurationMiddleware;
+use SentryLaravelIntegration;
+use SymfonyComponentHttpFoundationRequest;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
